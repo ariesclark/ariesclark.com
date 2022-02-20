8631ae66e728a5f342149e7e7bac9c8ea25de6ee
rubybb <10256477+rubybb@users.noreply.github.com>
2022-02-20 15:27:36 -0700

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -1,4 +1,46 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  headers: async () => {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on"
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload"
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block"
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN"
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=(), interest-cohort=()"
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff"
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin"
+          },
+          {
+            key: "Content-Security-Policy",
+            // https://csp-evaluator.withgoogle.com/
+            value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' https:; img-src 'self' https: data:; object-src 'none'; require-trusted-types-for 'script'"
+          }                                                                      
+        ],
+      },
+    ]
+  }
 }
