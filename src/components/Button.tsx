6b9c56ac76e345197f8ce1e350191fb96369aaa7
rubybb <10256477+rubybb@users.noreply.github.com>
2022-03-05 10:14:29 -0700

    fix: make button hover not secondary action

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,16 @@ import omit from "lodash/omit";
 
 import { IconArrowRight } from "./icons/ArrowRight";
 
-export type ButtonProps = Omit<HTMLAttributes<HTMLButtonElement>, "className"> & { name: string, icon?: React.ReactElement };
+export type ButtonProps = Omit<HTMLAttributes<HTMLButtonElement>, "className"> & {
+	name: string,
+	icon?: React.ReactElement
+};
+
 export const Button: React.VFC<ButtonProps> = function (props) {
 	const { name, icon } = props;
 
 	return (
-		<button {...omit(props, ["name", "icon"])} type="button" className="flex justify-between w-full px-4 py-2 text-black bg-white border-white rounded hover:text-white group hover:border hover:bg-transparent hover:scale-110">
+		<button {...omit(props, ["name", "icon"])} type="button" className="flex justify-between w-full px-4 py-2 text-black bg-white rounded hover:brightness-90 hover:bg-red-400 group">
 			<span>{name}</span>
 			{icon || (<IconArrowRight className="h-4 my-auto" />)}
 		</button>
