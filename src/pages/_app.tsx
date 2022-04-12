791ba9afa4fb8fbbdf3d23d6248005b6485d2d92
rubybb <10256477+rubybb@users.noreply.github.com>
2022-04-11 23:39:29 -0600

    feat: :poop: improve user experience

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable no-console */
 import "../../styles/globals.scss";
 import Script from "next/script";
+import { useEffect } from "react";
+import { random } from "@ariesclark/utils";
 
 import type { AppProps } from "next/app";
 
+let played: boolean = false;
 export default function App ({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		if (random.range(0, 1) === 1) {
+			console.log("Huh, what was that?");
+
+			document.addEventListener("click", () => {
+				setTimeout(() => {
+					if (played) return;
+					new Audio("/DiscordPingEffect.mp3").play();
+					played = true;
+				}, random.range(500, 2000));
+			});
+		}
+	}, []);
+
 	return (
 		<>
 			<Script
